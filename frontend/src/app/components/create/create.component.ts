94ebb1dfed4e7dbe98efa9573a9ce3aa0df54c5b
Cool232777 <cristian.azu20@gmail.com>
2022-08-01 14:11:32 -0600

    Definicion de metodos en el servicio de usuarios, inyeccion del servicio en los componentes

--- frontend/src/app/components/create/create.component.ts
+++ frontend/src/app/components/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-create',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private usersService: UsersService) { }
 
   ngOnInit(): void {
   }
--- frontend/src/app/services/users.service.ts
+++ frontend/src/app/services/users.service.ts
@@ -1,9 +1,45 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  
+  url = 'http://localhost:3000'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllUsers() {
+    return this.http.get(`${this.url}/users`);
+  }
+
+  getUserById() {
+    return this.http.get(`${this.url}/users/:id`);
+  }
+
+  createUser(username: string, firstName: string, lastName: string, status: string){
+    const User = {
+      username: username,
+      firstName: firstName,
+      lastName: lastName,
+      status: status
+    };
+    return this.http.post(`${this.url}/usesrs`, User)
+  }
+
+  updateUser(id: string, username: string, firstName: string, lastName: string, status: string){
+    const User = {
+      username: username,
+      firstName: firstName,
+      lastName: lastName,
+      status: status
+    };
+    return this.http.post(`${this.url}/usesrs/${id}`, User)
+  }
+
+  deleteUser(id: string){
+    return this.http.delete(`${this.url}/usesrs/${id}`)
+  }
+
 }
